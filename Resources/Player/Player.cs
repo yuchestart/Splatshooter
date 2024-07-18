4a3e283721c13f586c401316a7a072ed2e865bac
Che Yu <99137611+yuchestart@users.noreply.github.com>
2024-07-18 16:36:07 -0500

    Begin migrating player from RigidBody3D to CharacterBody3D
    I changed the type of root node that the player used, however I'm not done coding the player yet.

--- Resources/Player/Player.cs
+++ Resources/Player/Player.cs
@@ -1,8 +1,14 @@
 using Godot;
 using System;
 
-public partial class Player : RigidBody3D
+public partial class Player : CharacterBody3D
 {
+
+	[Export]
+	private float MoveForce = 1.0f;
+
+	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,7 +19,7 @@ public partial class Player : RigidBody3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		PlayerMovement(delta);
 
 
 	}
@@ -22,10 +28,22 @@ public partial class Player : RigidBody3D
 	{
 		if (Input.IsActionPressed("movement_move_forward"))
 		{
-			this.ApplyForce(new Vector3(0.0f, 0.0f, 1.0f));
+			this.ApplyForce(new Vector3(0.0f, 0.0f, -1 * MoveForce));
 		} else if (Input.IsActionPressed("movement_move_backward"))
 		{
-			this.ApplyForce(new Vector)
+			this.ApplyForce(new Vector3(0.0f, 0.0f, MoveForce));
 		}
-	}
+		if (Input.IsActionPressed("movement_move_left"))
+		{
+			this.ApplyForce(new Vector3(-1 * MoveForce, 0.0f, 0.0f));
+		}
+        if (Input.IsActionPressed("movement_move_right"))
+        {
+            this.ApplyForce(new Vector3(MoveForce, 0.0f, 0.0f));
+        }
+		if (Input.IsActionPressed("movement_jump"))
+		{
+			this.ApplyForce(new Vector3(0.0))
+		}
+    }
 }
