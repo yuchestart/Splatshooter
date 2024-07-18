7fbf9f6434c3e059414238d0618571b4e87039e0
Che Yu <99137611+yuchestart@users.noreply.github.com>
2024-07-18 15:00:27 -0500

    Add the stuff to the main scene
    Added MapLoader to the scene(currently just a container to hold a map, in this case PlaceholderMap)
    Added the player to the scene

---
+++ MapLoader.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class MapLoader : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//Right now MapLoader just loads placeholder map
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
