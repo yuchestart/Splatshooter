09ce4f0613fb634b1b771cc3d9bd3e632f948d2b
Che Yu <99137611+yuchestart@users.noreply.github.com>
2024-08-05 16:29:18 -0500

    Add game state management, or whatever it's called

---
+++ App.cs
@@ -0,0 +1,25 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TODO: Come up with a better name for this
+public enum GameStateEnum
+{
+    Menu,
+    InGame
+}
+
+//This class pretty much stores the app's state.
+public static class App
+{
+    public static bool IsPaused = false;
+
+    //TODO: Come up with a better name for this variable.
+    public static GameStateEnum GameState = GameStateEnum.InGame;
+
+}
+
+
