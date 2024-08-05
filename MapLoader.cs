291b01126c294adfec19dbd0741e0ec69510c14c
Che Yu <99137611+yuchestart@users.noreply.github.com>
2024-08-05 16:33:09 -0500

    I have no frickin clue what I'm doing here

--- MapLoader.cs
+++ MapLoader.cs
@@ -3,14 +3,18 @@ using System;
 
 public partial class MapLoader : Node3D
 {
+
+	Node3D LoadedMap;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		//Right now MapLoader just loads placeholder map
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+
+	public void LoadMap(string MapName)
 	{
+
 	}
 }
