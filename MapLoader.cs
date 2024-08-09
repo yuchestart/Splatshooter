0f2893d52e5d3515a24179bbb79b3b844198d41b
Che Yu <99137611+yuchestart@users.noreply.github.com>
2024-08-09 17:08:03 -0500

    Add MapLoader

--- MapLoader.cs
+++ MapLoader.cs
@@ -4,17 +4,30 @@ using System;
 public partial class MapLoader : Node3D
 {
 
-	Node3D LoadedMap;
+    private Node3D? CurrentMap;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		//Right now MapLoader just loads placeholder map
+		//TODO: Remove this call once things are good to go
+		LoadMap("PlaceholderMap");
 	}
 
 
-	public void LoadMap(string MapName)
+	public Node3D LoadMap(string MapName)
 	{
+		if(!(CurrentMap == null))
+		{
+            CurrentMap.QueueFree();
+        }
+		
+		PackedScene scene = ResourceLoader.Load<PackedScene>("res://Maps/" + MapName + "/" + MapName + ".tscn");
 
+		CurrentMap = scene.Instantiate<Node3D>();
+
+		AddChild(CurrentMap);
+
+		return CurrentMap;
 	}
 }
