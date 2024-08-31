18dd4fdb65c1d98bdf34bc2e906a631e002c4b26
Che Yu <99137611+yuchestart@users.noreply.github.com>
2024-08-30 19:26:06 -0500

    Added Game.cs
    In Gameplay.tscn, this script is to be added to Root to manage the scene ig ¯\_ (ツ)_/¯

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class Game : Node3D 
+{
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _Process(double delta)
+    {
+
+    }
+}
+
