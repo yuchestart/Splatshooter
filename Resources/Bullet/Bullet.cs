3dcc67e12f5005c4e3071dfa153a493096c51b6b
Che Yu <99137611+yuchestart@users.noreply.github.com>
2024-09-14 17:10:26 -0500

    Added weapons

--- Resources/Bullet/Bullet.cs
+++ Resources/Bullet/Bullet.cs
@@ -5,8 +5,11 @@ using System.Diagnostics;
 public partial class Bullet : Area3D
 {
 
+    private static PackedScene template;
+
     [Export]
-    public float bulletSize = 0; //Bullet sizes will be in diameter, not radius.
+    ///The diameter of the bullet in meters.
+    public float bulletSize = 0;
     [Export]
     public Vector3 bulletInitialVelocity = new Vector3(0, 0, 0);
     [Export]
@@ -14,11 +17,42 @@ public partial class Bullet : Area3D
     [Export]
     public float gravityAcceleration = -ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
+    public Guid firedBy; //The player that fired the bullet.
+    public Guid team; //TODO: Implement teams
+
     private CollisionShape3D collider;
     private MeshInstance3D meshInstance;
     private Vector3 _velocity;
+    private float mass;
 
 
+    public static Bullet CreateBullet()
+    {
+        if(template == null)
+        {
+            template = GD.Load<PackedScene>("res://Resources/Bullet/Bullet.tscn");
+        }
+        Bullet bullet = (Bullet)template.Instantiate();
+        return bullet;
+    }
+    /// <summary>
+    /// Add this bullet to the same scene tree as an object
+    /// As in, place the bullet in the "universe" that <c>item</c> is in.
+    /// </summary>
+    /// <param name="item">The object that the bullet will "share" a "universe" with. </param>
+    public void AddToGameplay(Node item)
+    {
+        Node root = item.GetTree().Root;
+        Node3D bulletContainer = root.GetNode<Node3D>("/root/Gameplay/BulletsContainer");
+        if (bulletContainer == null)
+        {
+            GD.Print("Failed to find the bullet container.");
+            return;
+        }
+        bulletContainer.AddChild(this);
+    }
+
+    #region Godot functions
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -44,6 +78,8 @@ public partial class Bullet : Area3D
 
         _velocity = bulletInitialVelocity;
 
+        AddToGroup("bullets");
+
 
     }
 
@@ -61,18 +97,42 @@ public partial class Bullet : Area3D
     {
 
     }
+    #endregion
+
+    private void InitializeMesh()
+    {
+        meshInstance = GetNode<MeshInstance3D>("Mesh");
+        BaseMaterial3D material = new StandardMaterial3D();
+        SphereMesh sphereMesh = new();
+        sphereMesh.Radius = bulletSize / 2;
+        sphereMesh.Height = bulletSize;
+
+        material.AlbedoColor = bulletColor;
+
+
+        sphereMesh.Material = material;
+        meshInstance.Mesh = sphereMesh;
+    }
+
 
     private void OnBodyEntered(Node3D body)
     {
+        //return; //Currently do nothing
         if (body.GetGroups().Contains("players"))
         {
             Player player = (Player)body;
+            if (player.UUID == firedBy) //Prevent the player from dying to their own shot
+                return;
             player.TakeDamage(bulletSize); //TODO: Implement accurate bullet damage based on bullet size. See Descriptive #1 for more details.
         }
-        else
+        if (body.GetGroups().Contains("bullets"))
         {
-            //TODO: Add splat effect and stuff idk
-            QueueFree();
+            //TODO: Handle bullet-bullet collisions.
+            //As of now, they just pass right through eachother like thin air.
+            return;
         }
+        //TODO: Add splat effect and stuff idk
+        QueueFree();
+
     }
 }
--- Resources/Weapons/Weapon.cs
+++ Resources/Weapons/Weapon.cs
@@ -7,17 +7,33 @@ using System.Threading.Tasks;
 
 public abstract partial class Weapon : Node
 {
-    public override void _Ready()
+
+    protected Player parent;
+
+    public Weapon(Player parent)
     {
+        this.parent = parent;
     }
 
-    public override void _Process(double delta)
+    public override void _Ready()
     {
+        Create();
+    }
+
+    public void HandleActions()
+    {
+        PrimaryFire();
+        SecondaryFire();
+        Reload();
     }
 
     //NOTE: To allow for more flexible behavior, these methods will be run each frame. The input handling will be offloaded to the extendee.
     public abstract void PrimaryFire();
     public abstract void SecondaryFire();
     public abstract void Reload();
-    
+
+    /// <summary>
+    /// When the weapon enters the scene tree, this function is called to instantiate things like the weapon mesh, HUD, effects, etc. etc.
+    /// </summary>
+    public abstract void Create();
 }
